8cde6194e8c9dcb5e47e51d1b75ee00ad5be3bf9
GossiperLoturot <ts20030211@gmail.com>
2026-01-22 10:54:50 +0900

    re-fine spatial index performance

--- native-core/src/geom/broad_tree.rs
+++ native-core/src/geom/broad_tree.rs
@@ -2,34 +2,130 @@ use glam::*;
 
 use super::*;
 
-type Rect = rstar::primitives::Rectangle<[i32; 2]>;
-type Node<T> = rstar::primitives::GeomWithData<Rect, T>;
-
 #[derive(Debug, Default)]
 pub struct BroadTree<T> {
-    rtree: rstar::RTree<Node<T>>
+    grid_s: ahash::AHashMap<IVec2, ahash::AHashSet<T>>,
+    grid_m: ahash::AHashMap<IVec2, ahash::AHashSet<T>>,
+    grid_l: ahash::AHashMap<IVec2, ahash::AHashSet<T>>,
 }
 
-impl<T> BroadTree<T> where T: PartialEq + Copy {
+impl<T> BroadTree<T> where T: Copy + std::hash::Hash + Eq {
+    const S_SIZE: u32 = 8;
+    const M_SIZE: u32 = 32;
+    const L_SIZE: u32 = 128;
+
     pub fn new() -> Self {
-        Self { rtree: Default::default() }
+        Self {
+            grid_s: Default::default(),
+            grid_m: Default::default(),
+            grid_l: Default::default(),
+        }
     }
 
     pub fn insert(&mut self, rect: IRect2, data: T) {
-        let rect = Rect::from_corners(rect.min.into(), rect.max.into());
-        let node = Node::new(rect, data);
-        self.rtree.insert(node);
+        let size = rect.size().max_element() as u32;
+
+        // small
+        if size <= Self::S_SIZE {
+            let min = rect.min.div_euclid(IVec2::splat(Self::S_SIZE as i32));
+            let max = rect.max.div_euclid(IVec2::splat(Self::S_SIZE as i32));
+            for y in min.y..=max.y {
+                for x in min.x..=max.x {
+                    let coord = IVec2::new(x, y);
+                    let grid = self.grid_s.entry(coord).or_default();
+                    grid.insert(data);
+                }
+            }
+        // medium
+        } else if size <= Self::M_SIZE {
+            let min = rect.min.div_euclid(IVec2::splat(Self::M_SIZE as i32));
+            let max = rect.max.div_euclid(IVec2::splat(Self::M_SIZE as i32));
+            for y in min.y..=max.y {
+                for x in min.x..=max.x {
+                    let coord = IVec2::new(x, y);
+                    let grid = self.grid_m.entry(coord).or_default();
+                    grid.insert(data);
+                }
+            }
+        // large
+        } else {
+            let min = rect.min.div_euclid(IVec2::splat(Self::L_SIZE as i32));
+            let max = rect.max.div_euclid(IVec2::splat(Self::L_SIZE as i32));
+            for y in min.y..=max.y {
+                for x in min.x..=max.x {
+                    let coord = IVec2::new(x, y);
+                    let grid = self.grid_l.entry(coord).or_default();
+                    grid.insert(data);
+                }
+            }
+        }
     }
 
     pub fn remove(&mut self, rect: IRect2, data: T) {
-        let rect = Rect::from_corners(rect.min.into(), rect.max.into());
-        let node = Node::new(rect, data);
-        self.rtree.remove(&node);
+        let size = rect.size().max_element() as u32;
+
+        // small
+        if size <= Self::S_SIZE {
+            let min = rect.min.div_euclid(IVec2::splat(Self::S_SIZE as i32));
+            let max = rect.max.div_euclid(IVec2::splat(Self::S_SIZE as i32));
+            for y in min.y..=max.y {
+                for x in min.x..=max.x {
+                    let coord = IVec2::new(x, y);
+                    self.grid_s.get_mut(&coord).map(|grid| grid.remove(&data));
+                }
+            }
+        // medium
+        } else if size <= Self::M_SIZE {
+            let min = rect.min.div_euclid(IVec2::splat(Self::M_SIZE as i32));
+            let max = rect.max.div_euclid(IVec2::splat(Self::M_SIZE as i32));
+            for y in min.y..=max.y {
+                for x in min.x..=max.x {
+                    let coord = IVec2::new(x, y);
+                    self.grid_m.get_mut(&coord).map(|grid| grid.remove(&data));
+                }
+            }
+        // large
+        } else {
+            let min = rect.min.div_euclid(IVec2::splat(Self::L_SIZE as i32));
+            let max = rect.max.div_euclid(IVec2::splat(Self::L_SIZE as i32));
+            for y in min.y..=max.y {
+                for x in min.x..=max.x {
+                    let coord = IVec2::new(x, y);
+                    self.grid_l.get_mut(&coord).map(|grid| grid.remove(&data));
+                }
+            }
+        }
     }
 
     pub fn find(&self, rect: IRect2) -> impl Iterator<Item = T> + '_ {
-        let rect = Rect::from_corners(rect.min.into(), rect.max.into());
-        let envelope = rstar::RTreeObject::envelope(&rect);
-        self.rtree.locate_in_envelope_intersecting(&envelope).map(|obj| obj.data)
+        // small
+        let min = rect.min.div_euclid(IVec2::splat(Self::S_SIZE as i32));
+        let max = rect.max.div_euclid(IVec2::splat(Self::S_SIZE as i32));
+        let iter_s = (min.y..=max.y).flat_map(move |y| {
+            (min.x..=max.x).flat_map(move |x| {
+                let coord = IVec2::new(x, y);
+                self.grid_s.get(&coord).into_iter().flat_map(|grid| grid.iter().copied())
+            })
+        });
+        // medium
+        let min = rect.min.div_euclid(IVec2::splat(Self::M_SIZE as i32));
+        let max = rect.max.div_euclid(IVec2::splat(Self::M_SIZE as i32));
+        let iter_m = (min.y..=max.y).flat_map(move |y| {
+            (min.x..=max.x).flat_map(move |x| {
+                let coord = IVec2::new(x, y);
+                self.grid_m.get(&coord).into_iter().flat_map(|grid| grid.iter().copied())
+            })
+        });
+        // large
+        let min = rect.min.div_euclid(IVec2::splat(Self::L_SIZE as i32));
+        let max = rect.max.div_euclid(IVec2::splat(Self::L_SIZE as i32));
+        let iter_l = (min.y..=max.y).flat_map(move |y| {
+            (min.x..=max.x).flat_map(move |x| {
+                let coord = IVec2::new(x, y);
+                self.grid_l.get(&coord).into_iter().flat_map(|grid| grid.iter().copied())
+            })
+        });
+
+        iter_s.chain(iter_m).chain(iter_l)
     }
 }
