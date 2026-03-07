0687793ad50795a85b774af120da591581327cb5
Takumi Sugimoto <ts20030211@gmail.com>
2026-03-07 16:49:55 +0900

    remove unused addons

--- native-main/src/addon/mod.rs
+++ native-main/src/addon/mod.rs
@@ -1,19 +1,3 @@
-// pub use animal::*;
-// pub use breakable::*;
-// pub use forwarder::*;
-// pub use generator::*;
-// pub use item_entity::*;
-// pub use particle::*;
-// pub use player::*;
-//
-// mod animal;
-// mod breakable;
-// mod forwarder;
-// mod generator;
-// mod item_entity;
-// mod particle;
-// mod player;
-
 pub use animal::*;
 pub use generator::*;
 pub use player::*;
